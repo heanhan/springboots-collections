a76064086d8b38198fd4b2641ba2c43fe207afe7
zhaojianhua <1763124707@qq.com>
2021-09-03 23:46:37 +0800

    gitlab 提交 日志   提交人：赵建华   提交日期：2021-09-03   提交类型：新增   提交内容：新建一个新的项目框架，用于日常学习。

---
+++ springboot-jenkisn-jar/src/main/java/com/example/springbootjenkisnjar/SpringbootJenkisnJarApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjenkisnjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkisnJarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJenkisnJarApplication.class, args);
+    }
+
+}
